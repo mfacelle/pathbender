1891e8e0b3aa054df7ef309197e6bc03056039db
Mike Facelle <mfacelle@gmail.com>
2016-04-09 16:47:05 -0400

    some work on level loading, test level scene, objects, etc

---
+++ Pathbender/Assets/scripts/level/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+// loads a level based on the saved PlayerPref
+public class LevelLoader : Singleton<LevelLoader> 
+{
+	public static string LOAD_LEVEL_KEY = "level.to.load";
+
+	private const string LEVEL_PREFIX = "level.";
+	private const int DEFAULT_LEVEL_NUM = 0;
+
+	public int debugLevelLoad;
+
+	// ---
+
+	void Start() {
+		// load debug level selection (set in editor)
+		if (DebugManager.Instance.isDebug) {
+			SceneManager.LoadScene(LEVEL_PREFIX + debugLevelLoad, LoadSceneMode.Additive);
+		}
+		// no scene to load - load the test level.0
+		else if (!PlayerPrefs.HasKey(LOAD_LEVEL_KEY) || PlayerPrefs.GetInt(LOAD_LEVEL_KEY) == 0) {
+			SceneManager.LoadScene(LEVEL_PREFIX + DEFAULT_LEVEL_NUM, LoadSceneMode.Additive);
+		}
+		// load level pointed to by player pref
+		else {
+			SceneManager.LoadScene(LEVEL_PREFIX + PlayerPrefs.GetInt(LOAD_LEVEL_KEY), LoadSceneMode.Additive);
+		}
+	}
+}
---
+++ Pathbender/Assets/scripts/objects/ChargedProjectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// a charged object that is used as a projectile
+// used by the "player" 
+public class ChargedProjectile : ChargedObject
+{
+	private Rigidbody2D body;
+
+	// ---
+
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	// ---
+
+	public void ApplyForce(Vector2 force) {
+		body.AddForce(force, ForceMode2D.Impulse);
+	}
+}
